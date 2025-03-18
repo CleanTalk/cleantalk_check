cfba4e0440605ed27f31fec17dce7866462225f3
alexandergull <alex.g@cleantalk.org>
2025-03-18 17:36:40 +0500

    Update form_handler.php to use a placeholder API key and enable optional parameters

--- example/form_handler.php
+++ example/form_handler.php
@@ -3,16 +3,16 @@
 use CleanTalkCheck\CleanTalkCheck;
 require_once '../src/autoload.php';
 
-$cleanTalkCheck = new CleanTalkCheck('a3asa2aby9azazy');
+$cleanTalkCheck = new CleanTalkCheck('your_api_key');
 $verdict = $cleanTalkCheck
     ->setEventToken($_POST[$cleanTalkCheck::EVENT_TOKEN_FIELD_NAME]) //obligatory
     ->setFormStartTime($_POST[$cleanTalkCheck::FORM_START_TIME_FIELD_NAME]) //obligatory
     ->setIP() //obligatory
-//    ->setEmail($_POST['email']) //optional
-//    ->setNickName($_POST['username']) //optional
-//    ->setMessage($_POST['message']) //optional
-//    ->useContactFormCheck() //optional
-//    ->setDoBlockNoJSVisitor() //optional
+    ->setEmail($_POST['email']) //optional
+    ->setNickName($_POST['username']) //optional
+    ->setMessage($_POST['message']) //optional
+    ->useContactFormCheck() //optional
+    ->setDoBlockNoJSVisitor() //optional
     ->getVerdict();
 
 //debug
