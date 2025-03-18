c93de94b81bf68cb1c9a993625a378be83f03638
alexandergull <alex.g@cleantalk.org>
2025-03-18 16:25:57 +0500

    Initial commit

---
+++ example/contact_form_page.php
@@ -0,0 +1,38 @@
+<?php
+
+use CleanTalkCheck\CleanTalkCheck;
+
+require_once('../autoload.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+    <h2>Contact Us</h2>
+    <form method="post" action="form_handler.php">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="username" placeholder="Enter your name">
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <input type="text" class="form-control" id="message" name="message"  placeholder="Enter your message">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <?php echo CleanTalkCheck::getFrontendHTMLCode()?>
+    </form>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+</body>
+</html>
---
+++ example/form_handler.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../autoload.php';
+
+$cleanTalkCheck = new CleanTalkCheck\CleanTalkCheck('a3asa2aby9azazy');
+$verdict = $cleanTalkCheck
+    ->setEventToken($_POST[$cleanTalkCheck::EVENT_TOKEN_FIELD_NAME]) //obligatory
+    ->setFormStartTime($_POST[$cleanTalkCheck::FORM_START_TIME_FIELD_NAME]) //obligatory
+    ->setIP() //obligatory
+//    ->setEmail($_POST['email']) //optional
+//    ->setNickName($_POST['username']) //optional
+//    ->setMessage($_POST['message']) //optional
+//    ->useContactFormCheck() //optional
+//    ->setDoBlockNoJSVisitor() //optional
+    ->getVerdict();
+
+//debug
+$cleanTalkCheck->whatsWrong();
+
+if (!$verdict->error) {
+    if (!$verdict->allowed) {
+        die('Message blocked: ' . $verdict->comment);
+    }
+}
+
+
+
+die('Message sent');
+//or anything you want to do
